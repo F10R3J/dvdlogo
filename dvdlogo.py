39fd54f68554246cd46aa48418b973bc704dcb53
F10R3J <60784740+F10R3J@users.noreply.github.com>
2020-06-29 20:06:02 -0700

    Add files via upload

---
+++ dvdlogo.py
@@ -0,0 +1,49 @@
+import turtle
+screen = turtle.Screen()
+screen.bgcolor("blue")
+screen.setup(width = 1920, height = 1200)
+# dvd logo
+screen.register_shape('dvdlogo.gif')
+dvd = turtle.Turtle()
+dvd.speed(0)
+dvd.shape("dvdlogo.gif")
+dvd.color("black")
+dvd.penup()
+dvd.goto(0,0)
+speed = 5
+dvd.dx = speed
+dvd.dy = speed
+cornerhits = 0
+
+# main loop
+loop = True
+while (loop == True):
+    screen.update()
+
+    dvd.setx(dvd.xcor() + dvd.dx)
+    dvd.sety(dvd.ycor() + dvd.dy)
+
+    if (dvd.xcor() >= 950):
+        dvd.dx = dvd.dx * -1
+
+    if (dvd.xcor() <= -950):
+        dvd.dx = dvd.dx * -1
+
+    if (dvd.ycor() >= 590):
+        dvd.dy = dvd.dy * -1
+
+    if (dvd.ycor() <= -590):
+        dvd.dy = dvd.dy * -1
+
+
+    if (dvd.xcor() >= 950) and (dvd.ycor() >= 590):
+        cornerhits = cornerhits + 1
+
+    if (dvd.xcor() >= 950) and (dvd.ycor() <= -590):
+        cornerhits = cornerhits + 1
+
+    if (dvd.xcor() <= -950) and (dvd.ycor() >= 590):
+        cornerhits = cornerhits + 1
+
+    if (dvd.xcor() <= -950) and (dvd.ycor() <= -590):
+        cornerhits = cornerhits + 1
